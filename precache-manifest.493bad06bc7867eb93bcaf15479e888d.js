1d2684adbb5fdd2dac69e4da413b49b80d9f935d
Jordan <jrdnvan@gmail.com>
2020-01-04 22:25:40 +1100

    Updates

---
+++ precache-manifest.493bad06bc7867eb93bcaf15479e888d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9464a3af45dbd7335c3da90cca88bbf",
+    "url": "/calendar-planner/index.html"
+  },
+  {
+    "revision": "c3fd62475a531a4b5c2c",
+    "url": "/calendar-planner/static/css/main.e18fd3c8.chunk.css"
+  },
+  {
+    "revision": "da956519523ae064def9",
+    "url": "/calendar-planner/static/js/2.05301dda.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/calendar-planner/static/js/2.05301dda.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c3fd62475a531a4b5c2c",
+    "url": "/calendar-planner/static/js/main.4ce48f03.chunk.js"
+  },
+  {
+    "revision": "be7e3db039b17e629c43",
+    "url": "/calendar-planner/static/js/runtime-main.c6bc734f.js"
+  },
+  {
+    "revision": "6fa01a90dfa1de406f42647e04ee646a",
+    "url": "/calendar-planner/static/media/bin-icon.6fa01a90.png"
+  }
+]);
